f0978b93ce00f3309fac0d7924636b1342af8747
baxtree <xi.bai.ed@gmail.com>
2022-01-17 17:20:37 +0000

    support in converting multiple icd codes

--- README.md
+++ README.md
@@ -1,21 +1,32 @@
 # icd_code_converter
-Command line interface for converting between ICD-9 codes and ICD-10 codes.
+Command line interface for converting between ICD-9 codes and ICD-10 codes. It functions as a thin wrapper of [touch.icd_map](https://hub.wwenjie.org/touch/).
 
 # Prerequisites
 - The [R language and environment](https://cloud.r-project.org/) has been downloaded and installed.
 
 - Your login user needs to have the permission for installing R libraries. Otherwise, try `sudo usermod -a -G staff $USER`.
 
+# Examples
+```
+$ ./icd_code_converter --icd9 "401.9"                       # I10,I169
+$ ./icd_code_converter --icd10 "I10;I169"                   # 4010,4011,4019;4019
+$ ./icd_code_converter --icd9 "401.9=001.1" --delimiter "=" # I10,I169=A001
+```
+
 # Usage
 ```
 usage: icd_code_converter [--] [--help] [--icd9 ICD9] [--icd10 ICD10]
+       [--delimiter DELIMITER]
 
 Convert between ICD-9 codes and ICD-10 codes
 
 flags:
-  -h, --help   show this help message and exit
+  -h, --help       show this help message and exit
 
 optional arguments:
-  -n, --icd9   The ICD-9 code
-  -t, --icd10  The ICD-10 code
+  -n, --icd9       single ICD-9 code or semicolon-separated ICD-9 codes
+  -t, --icd10      single ICD-10 code or semicolon-separated ICD-10
+                   codes
+  -d, --delimiter  symbol for separating ICD codes and cannot be ','
+                   [default: ;]
 ```
--- tests/test.py
+++ tests/test.py
@@ -16,33 +16,67 @@ class IcdCodeConverterTests(unittest.TestCase):
 
         ps = subprocess.Popen([self.command_path, "--icd9", icd9_code], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         output = ps.communicate()[0].decode()
-        
+
         self.assertTrue(isinstance(output, str))
-        self.assertEqual(["I10", "I169"], output.split(","))
-    
+        self.assertEqual(["I10,I169"], output.split(";"))
+
     def test_convert_icd9_no_mapping(self) -> None:
         invalid_code = "401.99999"
 
         ps = subprocess.Popen([self.command_path, "--icd9", invalid_code], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         output = ps.communicate()[0].decode()
-        
+
         self.assertTrue(isinstance(output, str))
-        self.assertEqual(["NA"], output.split(","))
-    
+        self.assertEqual(["NA"], output.split(";"))
+
+    def test_convert_multiple_icd9_codes(self) -> None:
+        icd9_codes = "401.9;001.1"
+
+        ps = subprocess.Popen([self.command_path, "--icd9", icd9_codes], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        output = ps.communicate()[0].decode()
+
+        self.assertTrue(isinstance(output, str))
+        self.assertEqual(["I10,I169", "A001"], output.split(";"))
+
+    def test_custom_delimiter_for_icd9(self) -> None:
+        icd9_code = "401.9=001.1"
+
+        ps = subprocess.Popen([self.command_path, "--icd9", icd9_code, "--delimiter", "="], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        output = ps.communicate()[0].decode()
+
+        self.assertEqual("I10,I169=A001", output)
+
     def test_convert_icd10(self) -> None:
         icd10_code = "I10"
 
         ps = subprocess.Popen([self.command_path, "--icd10", icd10_code], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         output = ps.communicate()[0].decode()
-        
+
         self.assertTrue(isinstance(output, str))
-        self.assertEqual(['4010', '4011', '4019'], output.split(","))
-    
+        self.assertEqual(["4010,4011,4019"], output.split(";"))
+
     def test_convert_icd10_no_mapping(self) -> None:
         invalid_code = "I100000000"
 
         ps = subprocess.Popen([self.command_path, "--icd10", invalid_code], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         output = ps.communicate()[0].decode()
-        
+
         self.assertTrue(isinstance(output, str))
-        self.assertEqual(["NA"], output.split(","))
+        self.assertEqual(["NA"], output.split(";"))
+
+    def test_convert_multiple_icd10_codes(self) -> None:
+        icd10_codes = "I10;I169"
+
+        ps = subprocess.Popen([self.command_path, "--icd10", icd10_codes], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        output = ps.communicate()[0].decode()
+
+        self.assertTrue(isinstance(output, str))
+        self.assertEqual(["4010,4011,4019", "4019"], output.split(";"))
+
+    def test_custom_delimiter_for_icd10(self) -> None:
+        icd10_code = "I10=I169"
+
+        ps = subprocess.Popen([self.command_path, "--icd10", icd10_code, "--delimiter", "="], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        output = ps.communicate()[0].decode()
+
+        self.assertEqual("4010,4011,4019=4019", output)
